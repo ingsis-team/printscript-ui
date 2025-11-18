b13fe79005536d5d1f4839e5d89b5aebe2cdc1f9
AnaB2 <ana.borda@ing.austral.edu.ar>
2025-11-18 20:30:17 -0300

    change dockerfile & nginx conifg

--- Dockerfile
+++ Dockerfile
@@ -13,3 +13,9 @@ COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
 
+
+
+
+
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,13 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      'process.env.FRONTEND_URL': JSON.stringify(env.FRONTEND_URL),
-      'process.env.BACKEND_URL': JSON.stringify(env.BACKEND_URL)
+      'process.env': JSON.stringify({
+        FRONTEND_URL: env.FRONTEND_URL || env.VITE_FRONTEND_URL || 'http://localhost:5173',
+        BACKEND_URL: env.BACKEND_URL || env.VITE_BACKEND_URL || 'http://localhost:8080/api',
+        PRINTSCRIPT_SERVICE_URL: env.PRINTSCRIPT_SERVICE_URL || env.VITE_PRINTSCRIPT_SERVICE_URL || 'http://localhost:8082',
+        AUTH0_USERNAME: env.AUTH0_USERNAME || env.VITE_AUTH0_USERNAME || '',
+        AUTH0_PASSWORD: env.AUTH0_PASSWORD || env.VITE_AUTH0_PASSWORD || '',
+      })
     },
     plugins: [react()],
   }
